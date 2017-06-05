f7f6d8948d3e3a2e877a3bf65219f8acba5dcbd4
SamarinS <samarin.sergey92@gmail.com>
2017-06-05 17:58:05 +0300

    Added support for float64 sparse matrices
    Now it is possible to call fit/predict methods
    with data in both dense and sparse forms.

--- cpm-svm/python/cpm_svm.py
+++ cpm-svm/python/cpm_svm.py
@@ -1,7 +1,12 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils import check_X_y, check_array
 import numpy as np
+import scipy.sparse as sp
 import spam
+
+def check_that_sparse_X_is_float64(X):
+    if X.dtype != np.float64:
+        raise ValueError("Sparse X with dtype=%s is not supported!" % X.dtype.type.__name__)
 
 class CPM_SVM(BaseEstimator, ClassifierMixin):
     def __init__(self, lambda_coef=0.01, epsilon_tol=1e-3, epsilon_abs=0.01, max_iter=100):
@@ -10,10 +15,15 @@ class CPM_SVM(BaseEstimator, ClassifierMixin):
         self.epsilon_tol = epsilon_tol
         self.epsilon_abs = epsilon_abs
         self.max_iter = max_iter
-    
+
     def fit(self, X, y):
-        X, y = check_X_y(X, y)
-        X = np.asarray(X, dtype=np.float64, order='C')
+
+        if sp.isspmatrix(X):
+            X, y = check_X_y(X, y, accept_sparse='csr')
+            check_that_sparse_X_is_float64(X)
+        else:
+            X, y = check_X_y(X, y)
+            X = np.asarray(X, dtype=np.float64, order='C')
 
         if issubclass(y.dtype.type, np.floating) == True:
             for label in y:
@@ -27,26 +37,47 @@ class CPM_SVM(BaseEstimator, ClassifierMixin):
                 "The number of classes has to be greater than one; got %d"
                 % len(self.classes_))
 
-        self.betta_ = spam.fit(X, y,
-            self.lambda_coef,
-            self.epsilon_abs,
-            self.epsilon_tol,
-            self.max_iter)
+        if sp.isspmatrix(X):
+            self.betta_ = spam.fit_sparse(X.shape[0], X.shape[1], X.data,
+                X.indices, X.indptr, y,
+                self.lambda_coef,
+                self.epsilon_abs,
+                self.epsilon_tol,
+                self.max_iter)
+        else:
+            self.betta_ = spam.fit(X, y,
+                self.lambda_coef,
+                self.epsilon_abs,
+                self.epsilon_tol,
+                self.max_iter)
 
         return self
+
+    def check_columns(self, n_X_columns):
+        n_features = self.betta_.shape[1]
+        if n_X_columns != n_features:
+            raise ValueError(
+                "The number of X columns has to be equal to the number of features (%d); got %d"
+                % (n_features, n_X_columns))
     
     def predict(self, X):
         if hasattr(self, 'betta_') == False:
             raise ValueError("The classifier is not fitted yet. Please call 'fit' with appropriate arguments before using this method.")
 
-        X = check_array(X)
-        X = np.asarray(X, dtype=np.float64, order='C')
+        if sp.isspmatrix(X):
+            X = check_array(X, accept_sparse='csr')
+            check_that_sparse_X_is_float64(X)
 
-        if X.shape[1] != self.betta_.shape[1]:
-            raise ValueError(
-                "The number of X columns has to be equal to the number of features (%d); got %d"
-                % (self.betta_.shape[1], X.shape[1]))
+            self.check_columns(X.shape[1])
 
-        y = spam.predict(X, self.betta_, len(self.classes_))
+            y = spam.predict_sparse(X.shape[0], X.shape[1], X.data,
+                X.indices, X.indptr, self.betta_, len(self.classes_))
+        else:
+            X = check_array(X)
+            X = np.asarray(X, dtype=np.float64, order='C')
+
+            self.check_columns(X.shape[1])
+
+            y = spam.predict(X, self.betta_, len(self.classes_))
 
         return self.classes_.take(np.asarray(y, dtype=np.intp))
--- cpm-svm/python/spammodule.c
+++ cpm-svm/python/spammodule.c
@@ -62,7 +62,7 @@ static PyObject* spam_fit(PyObject* self, PyObject* args)
         return NULL;
     }
 
-    BaseData* data = new DenseData(
+    DenseData data(
         (const double*)X->data,
         X->dimensions[0],
         X->dimensions[1],
@@ -72,7 +72,53 @@ static PyObject* spam_fit(PyObject* self, PyObject* args)
 
     SvmParams params(lambda, epsilon_abs, epsilon_tol, tMax);
     SvmData svmData;
-    trainSvm(*(DenseData*)data, (const long*)y->data, params, &svmData);
+    trainSvm(data, (const long*)y->data, params, &svmData);
+
+    auto betta = getBetta(svmData);
+    return PyArray_Return(betta);
+}
+
+static PyObject* spam_fit_sparse(PyObject* self, PyObject* args)
+{
+    int rows_number;
+    int cols_number;
+    PyArrayObject* X_data;
+    PyArrayObject* X_indices;
+    PyArrayObject* X_indptr;
+    PyArrayObject* y;
+    double lambda;
+    double epsilon_abs;
+    double epsilon_tol;
+    int tMax;
+
+    if (!PyArg_ParseTuple(args, "iiO!O!O!O!dddi",
+                          &rows_number,
+                          &cols_number,
+                          &PyArray_Type, &X_data,
+                          &PyArray_Type, &X_indices,
+                          &PyArray_Type, &X_indptr,
+                          &PyArray_Type, &y, 
+                          &lambda,
+                          &epsilon_abs,
+                          &epsilon_tol,
+                          &tMax))
+    {
+        return NULL;
+    }
+
+    SparseData data(
+        (const double*)X_data->data,
+        rows_number,
+        cols_number,
+        (int*)X_indices->data,
+        (int)X_indices->dimensions[0],
+        (int*)X_indptr->data,
+        (int)X_indptr->dimensions[0]
+    );
+
+    SvmParams params(lambda, epsilon_abs, epsilon_tol, tMax);
+    SvmData svmData;
+    trainSvm(data, (const long*)y->data, params, &svmData);
 
     auto betta = getBetta(svmData);
     return PyArray_Return(betta);
@@ -92,7 +138,7 @@ static PyObject* spam_predict(PyObject* self, PyObject* args)
         return NULL;
     }
 
-    BaseData* data = new DenseData(
+    DenseData data(
         (const double*)X->data,
         X->dimensions[0],
         X->dimensions[1],
@@ -109,14 +155,62 @@ static PyObject* spam_predict(PyObject* self, PyObject* args)
     setBetta(&svmData, betta);
     svmData.n_classes = n_classes;
 
-    predict(*(DenseData*)data, (long*)obj->data, svmData);
+    predict(data, (long*)obj->data, svmData);
+
+    return PyArray_Return(obj);
+}
+
+static PyObject* spam_predict_sparse(PyObject* self, PyObject* args)
+{
+    int rows_number;
+    int cols_number;
+    PyArrayObject* X_data;
+    PyArrayObject* X_indices;
+    PyArrayObject* X_indptr;
+    PyArrayObject* betta;
+    int n_classes;
+
+    if (!PyArg_ParseTuple(args, "iiO!O!O!O!i",
+                          &rows_number,
+                          &cols_number,
+                          &PyArray_Type, &X_data,
+                          &PyArray_Type, &X_indices,
+                          &PyArray_Type, &X_indptr,
+                          &PyArray_Type, &betta,
+                          &n_classes))
+    {
+        return NULL;
+    }
+
+    SparseData data(
+        (const double*)X_data->data,
+        rows_number,
+        cols_number,
+        (int*)X_indices->data,
+        (int)X_indices->dimensions[0],
+        (int*)X_indptr->data,
+        (int)X_indptr->dimensions[0]
+    );
+
+    int n_dimensions = 1;
+    npy_intp dimensions[n_dimensions] = {(npy_intp)rows_number};
+    PyArrayObject* obj = (PyArrayObject*)PyArray_SimpleNew(
+        n_dimensions, dimensions, PyArray_LONG);
+
+    SvmData svmData;
+    setBetta(&svmData, betta);
+    svmData.n_classes = n_classes;
+
+    predict(data, (long*)obj->data, svmData);
 
     return PyArray_Return(obj);
 }
 
 static PyMethodDef SpamMethods[] = {
     {"fit", spam_fit, METH_VARARGS, "cpm-svm classifier: fit"},
+    {"fit_sparse", spam_fit_sparse, METH_VARARGS, "cpm-svm classifier: fit sparse"},
     {"predict", spam_predict, METH_VARARGS, "cpm-svm classifier: predict"},
+    {"predict_sparse", spam_predict_sparse, METH_VARARGS, "cpm-svm classifier: predict sparse"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
